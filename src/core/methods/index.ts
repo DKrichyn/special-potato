e560c258451cf1643cf49277474153dbf1e5491d
Дмитро Кричун <144226692+DKrichyn@users.noreply.github.com>
2025-12-05 20:10:17 +0200

    Fix solver exports in methods index

--- src/core/methods/index.ts
+++ src/core/methods/index.ts
@@ -1,8 +1,8 @@
-export { solveWithCramer } from './cramer';
+export { solveCramer } from './cramer';
 export { solveGauss } from './gauss';
-export { solveWithGaussianElimination } from './gaussianElimination';
 export { solveGaussJordan, solveWithGaussJordan } from './gaussJordan';
-export { solveWithGaussSeidel } from './gaussSeidel';
-export { solveJacobi, solveWithJacobi } from './jacobi';
 export { solveSeidel } from './seidel';
+export { solveJacobi, solveWithJacobi } from './jacobi';
+export { solveWithGaussianElimination } from './gaussianElimination';
+export { solveWithGaussSeidel } from './gaussSeidel';
 export { checkSolution } from './checkSolution';
